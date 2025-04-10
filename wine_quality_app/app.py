beeb310dee0e4e43d34e95c4c6a8fd8ceea0a3a0
mayamorad <maya.z.morad@gmail.com>
2025-04-10 09:55:38 -0500

    Update app.py

--- wine_quality_app/app.py
+++ wine_quality_app/app.py
@@ -122,5 +122,5 @@ def contact():
 
 if __name__ == '__main__':
     from os import environ
-    port = int(environ.get("PORT", 5000))
+    port = int(environ.get("PORT", 5001))
     app.run(debug=False, host='0.0.0.0', port=port)
